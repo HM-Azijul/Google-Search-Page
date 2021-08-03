67868fbf44e2e8679c7ef6bf7f48448b7098e509
HM-Azijul <azijulhm@gmail.com>
2021-08-03 11:11:23 +0600

    fixed some mistake in code

---
+++ app.js
@@ -0,0 +1,7 @@
+const searchQuery = document.getElementById("search-query");
+const searchBtn = document.querySelector(".search-btn");
+
+searchBtn.addEventListener("click", () => {
+    let url = "https: //www.google.com/search?q=" + searchQuery.value;
+    window.open(url);
+});
